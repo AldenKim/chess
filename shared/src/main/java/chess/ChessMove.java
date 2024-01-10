f319c51c2ef920648df23409b167b789c0ebc847
AldenKim <aldenkim.ut@gmail.com>
2024-01-10 13:31:49 -0700

    Initialize(ChessMove) constructor

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -7,9 +7,14 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
-
+    private ChessPosition start;
+    private ChessPosition end;
+    private ChessPiece.PieceType promotionPiece;
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.start = startPosition;
+        this.end = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
