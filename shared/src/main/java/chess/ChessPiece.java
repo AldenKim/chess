62046078e7a8626a4fd21dc19ed688b545c5acc9
AldenKim <aldenkim.ut@gmail.com>
2024-01-10 12:24:31 -0700

    Add(pieceMoves) code, will need other functions to continue.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Represents a single chess piece
@@ -52,6 +53,14 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> validMoves = new HashSet<>();
+
+        switch(type){
+            case KING:
+
+                break;
+        }
+
+        return validMoves;
     }
 }
