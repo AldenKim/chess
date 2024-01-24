fe1bce9500985cb4efb4a693a393529b5100a4e2
AldenKim <aldenkim.ut@gmail.com>
2024-01-24 16:01:41 -0700

    Implement (setBoard, getBoard) getters and setters for the board

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -129,7 +129,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.chessBoard = board;
     }
 
     /**
@@ -138,7 +138,7 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return chessBoard;
     }
 
     private ChessPosition findKingPosition(TeamColor teamColor)
