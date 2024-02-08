4366453e657d1a2555a9b1ff40ba31a87c1b1ab5
AldenKim <aldenkim.ut@gmail.com>
2024-02-08 16:18:06 -0700

    Start creating the UserDAO

---
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -0,0 +1,35 @@
+package dataAccess;
+
+import model.GameData;
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class UserDAO implements DataAccess {
+    private Map<String, UserData> userDataMap;
+
+    public UserDAO(){
+        userDataMap = new HashMap<>();
+    }
+
+    public void clear() throws DataAccessException{
+        userDataMap.clear();
+    }
+
+    public void createUser(UserData user) throws DataAccessException{
+        if(userDataMap.containsKey(user.getUsername())){
+            throw new DataAccessException("User already exists with username: " + user.getUsername());
+        }
+        userDataMap.put(user.getUsername(), user);
+    }
+
+    public UserData getUser (String username) throws DataAccessException{
+        if(userDataMap.containsKey(username)){
+            return userDataMap.get(username);
+        }
+        else{
+            throw new DataAccessException("User not found: " + username);
+        }
+    }
+}
