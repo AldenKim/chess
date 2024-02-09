48a8d4ace89f7ded7b294c7de706761459a806fb
AldenKim <aldenkim.ut@gmail.com>
2024-02-09 14:44:24 -0700

    Add some logic for the LoginService

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,31 @@
 package service;
 
+import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+
 public class LoginService {
+
+    private UserDAO userDAO;
+
+    public LoginService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public AuthData login(UserData user) throws DataAccessException {
+        UserData storedUser = userDAO.getUser(user.getUsername());
+
+        if(!storedUser.password().equals(user.password())) {
+            throw new DataAccessException("Password is incorrect");
+        }
+
+        String authToken = generateAuthToken();
+
+        return new AuthData(authToken, user.getUsername());
+    }
+
+    private String generateAuthToken() {
+        return "authToken 200";
+    }
 }
