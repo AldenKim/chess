18954b45903e4ebf60a25d50be4bcd719c058460
AldenKim <aldenkim.ut@gmail.com>
2024-02-10 15:53:10 -0700

    Add more functions to service class

--- server/src/main/java/service/ChessService.java
+++ server/src/main/java/service/ChessService.java
@@ -4,6 +4,8 @@ import dataAccess.AuthDAO;
 import dataAccess.DataAccessException;
 import dataAccess.GameDAO;
 import dataAccess.UserDAO;
+import model.AuthData;
+import model.GameData;
 import model.UserData;
 
 public class ChessService {
@@ -32,5 +34,39 @@ public class ChessService {
 
     public void deleteUser(String username) throws DataAccessException {
         userDAO.deleteUser(username);
+    }
+
+    public GameData createGame(GameData game) throws DataAccessException {
+        return gameDAO.createGame(game);
+    }
+
+    public GameData getGame(String gameID) throws DataAccessException {
+        return gameDAO.getGame(gameID);
+    }
+
+    public GameData[] listGames() throws DataAccessException {
+        return gameDAO.listGames();
+    }
+
+    public void updateGame(String gameID, GameData updatedGameData) throws DataAccessException {
+        gameDAO.updateGame(gameID, updatedGameData);
+    }
+
+    public AuthData createAuth(AuthData auth) throws DataAccessException {
+        return authDAO.createAuth(auth);
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authDAO.getAuth(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authDAO.deleteAuth(authToken);
+    }
+
+    public void clear() throws DataAccessException {
+        userDAO.clear();
+        gameDAO.clear();
+        authDAO.clear();
     }
 }
