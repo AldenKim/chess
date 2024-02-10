454168790d8c6130535a00c495a9724e791bae3f
AldenKim <aldenkim.ut@gmail.com>
2024-02-10 15:31:43 -0700

    Reimplement AuthDAO as interface

---
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -0,0 +1,13 @@
+package dataAccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    void clear() throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(String authToken) throws DataAccessException;
+}
