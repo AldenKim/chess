c0d560d94eea0fdabc12658692e3cb97347b0033
AldenKim <aldenkim.ut@gmail.com>
2024-02-12 16:01:48 -0700

    Add logic for RegisterService class

---
+++ server/src/main/java/service/RegisterService.java
@@ -0,0 +1,43 @@
+package service;
+
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import requests.RegisterRequest;
+import results.RegisterResult;
+
+import java.security.AuthProvider;
+import java.util.UUID;
+
+public class RegisterService {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+
+    public RegisterService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws DataAccessException {
+        try {
+            if(userDAO.getUser(request.username()) != null) {
+                throw new DataAccessException("Error 403: Already Taken");
+            }
+
+            UserData newUser = new UserData(request.username(), request.password(), request.email());
+
+            String authToken = UUID.randomUUID().toString();
+            AuthData newAuth = new AuthData(authToken, newUser.username());
+
+            userDAO.createUser(newUser);
+            authDAO.createAuth(newAuth);
+
+            return new RegisterResult(newUser.username(), authToken);
+        }
+        catch (DataAccessException e) {
+            throw e;
+        }
+    }
+}
