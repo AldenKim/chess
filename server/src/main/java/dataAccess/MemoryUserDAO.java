79fdbe89ba0eebb9febab47e8d7b032dd02b0c3a
AldenKim <aldenkim.ut@gmail.com>
2024-02-16 08:51:18 -0700

    Implement MemoryUserDAO

---
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -0,0 +1,39 @@
+package dataAccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO{
+    private final HashMap<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException{
+        users.clear();
+    }
+
+    @Override
+    public UserData createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User exists already");
+        }
+
+        users.put(user.username(), user);
+        return user;
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void updateUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public void deleteUser(String username) throws DataAccessException {
+        users.remove(username);
+    }
+}
