48a77ffdc214693ff4ed6449a98861528fcdce4b
AldenKim <aldenkim.ut@gmail.com>
2024-02-18 00:17:48 -0700

    Implement MemoryAuthDAO

--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,5 +1,30 @@
 package dataAccess;
 
-public class MemoryAuthDAO implements AuthDAO{
+import model.AuthData;
 
+import java.util.HashMap;
+
+public class MemoryAuthDAO implements AuthDAO{
+    private final HashMap<String, AuthData> authDataMap = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        authDataMap.clear();
+    }
+
+    @Override
+    public AuthData createAuth(AuthData auth) throws DataAccessException {
+        authDataMap.put(auth.authToken(), auth);
+        return auth;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return authDataMap.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authDataMap.remove(authToken);
+    }
 }
