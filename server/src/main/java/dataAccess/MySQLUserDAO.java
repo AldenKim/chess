bb80615d68c41095c83330529eceef01502d2c59
AldenKim <aldenkim.ut@gmail.com>
2024-02-26 13:23:33 -0700

    Implement MySQLUserDAO draft

--- server/src/main/java/dataAccess/MySQLUserDAO.java
+++ server/src/main/java/dataAccess/MySQLUserDAO.java
@@ -1,4 +1,55 @@
 package dataAccess;
 
+import model.UserData;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MySQLUserDAO implements UserDAO{
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement statement = conn.prepareStatement("TRUNCATE TABLE users")) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error while clearing user data: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public UserData createUser(UserData user) throws DataAccessException {
+        try(Connection conn = DatabaseManager.getConnection();
+        PreparedStatement statement = conn.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)")) {
+            statement.setString(1, user.username());
+            statement.setString(2, user.password());
+            statement.setString(3, user.email());
+            statement.executeUpdate();
+            return user;
+        } catch (SQLException e) {
+            throw new DataAccessException("Error while creating user: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM users WHERE username = ?")) {
+            statement.setString(1, username);
+            try(ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String retrievedUsername = resultSet.getString("username");
+                    String password = resultSet.getString("password");
+                    String email = resultSet.getString("email");
+                    return new UserData(retrievedUsername, password, email);
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error while fetching user: " + e.getMessage());
+        }
+    }
 }
