111a17c05c2342baf2dff111d0ccf95d8df476e7
AldenKim <aldenkim.ut@gmail.com>
2024-03-25 12:45:25 -0600

    Add more code to WebSocketSessions and finish up

--- server/src/main/java/server/websocket/WebSocketSessions.java
+++ server/src/main/java/server/websocket/WebSocketSessions.java
@@ -4,6 +4,7 @@ import spark.Session;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class WebSocketSessions {
     private static final Map<Integer, Map<String, Session>> sessionMap = new HashMap<>();
@@ -23,5 +24,18 @@ public class WebSocketSessions {
         }
     }
 
+    public static void removeSession(Session session) {
+        Set<Map.Entry<Integer, Map<String, Session>>> entrySet = sessionMap.entrySet();
+        for(Map.Entry<Integer, Map<String, Session>> entry : entrySet) {
+            Map<String, Session> gameSessions = entry.getValue();
+            gameSessions.values().removeIf(s -> s.equals(session));
+            if(gameSessions.isEmpty()) {
+                entrySet.remove(entry);
+            }
+        }
+    }
 
+    public static Map<String, Session> getSessionsForGame(int gameID) {
+        return sessionMap.get(gameID);
+    }
 }
