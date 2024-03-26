2318b075f4a3f24a18340f98a53b29251c15be24
AldenKim <aldenkim.ut@gmail.com>
2024-03-26 16:24:05 -0600

    MakeMoveCommand inheriting from UserGameCommand

---
+++ shared/src/main/java/webSocketMessages/userCommands/MakeMoveCommand.java
@@ -0,0 +1,23 @@
+package webSocketMessages.userCommands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand{
+    private int gameID;
+    private ChessMove move;
+
+    public MakeMoveCommand(int gameID, ChessMove move, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.move = move;
+        this.commandType = CommandType.MAKE_MOVE;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+}
