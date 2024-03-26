b6cf9b74836d0445c504d19471ffc404c381b2e5
AldenKim <aldenkim.ut@gmail.com>
2024-03-26 16:30:06 -0600

    Add more subclasses inheriting from UserGameCommand based on the specs given

---
+++ shared/src/main/java/webSocketMessages/userCommands/LeaveCommand.java
@@ -0,0 +1,15 @@
+package webSocketMessages.userCommands;
+
+public class LeaveCommand extends UserGameCommand{
+    private int gameID;
+
+    public LeaveCommand(int gameID, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.commandType = CommandType.LEAVE;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+}
---
+++ shared/src/main/java/webSocketMessages/userCommands/ResignCommand.java
@@ -0,0 +1,15 @@
+package webSocketMessages.userCommands;
+
+public class ResignCommand extends UserGameCommand{
+    private int gameID;
+
+    public ResignCommand(int gameID, String authToken) {
+        super(authToken);
+        this.gameID = gameID;
+        this.commandType = CommandType.RESIGN;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+}
